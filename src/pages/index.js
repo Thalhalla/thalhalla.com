1c8218201321c70e16768bfeab9a242927d8efea
Josh Cox <josh@webhosting.coop>
2023-07-17 11:45:54 -0500

    simplification

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,91 +1,19 @@
-import * as React from "react"
+// Step 1: Import React
+import * as React from 'react'
 import hammerTime from "./hammerTime.jpg";
 
-const pageStyles = {
-  color: "#232129",
-  padding: 96,
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-const headingAccentStyles = {
-  color: "#663399",
-}
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
-const listStyles = {
-  marginBottom: 96,
-  paddingLeft: 0,
-}
-const listItemStyles = {
-  fontWeight: 300,
-  fontSize: 24,
-  maxWidth: 560,
-  marginBottom: 30,
-}
-
-const linkStyle = {
-  color: "#8954A8",
-  fontWeight: "bold",
-  fontSize: 16,
-  verticalAlign: "5%",
-}
-
-const docLinkStyle = {
-  ...linkStyle,
-  listStyleType: "none",
-  marginBottom: 24,
-}
-
-const descriptionStyle = {
-  color: "#232129",
-  fontSize: 14,
-  marginTop: 10,
-  marginBottom: 0,
-  lineHeight: 1.25,
-}
-
-const badgeStyle = {
-  color: "#fff",
-  backgroundColor: "#088413",
-  border: "1px solid #088413",
-  fontSize: 11,
-  fontWeight: "bold",
-  letterSpacing: 1,
-  borderRadius: 4,
-  padding: "4px 6px",
-  display: "inline-block",
-  position: "relative",
-  top: -2,
-  marginLeft: 10,
-  lineHeight: 1,
-}
-
+// Step 2: Define your component
 const IndexPage = () => {
   return (
-    <main style={pageStyles}>
-      <div className='hammerTime'>
-        <img src={hammerTime} height="242" alt="Thalhammer" className='Thor-center' />
-      </div>
-      <h1 style={headingStyles}>
-        Thalhalla
-        <br />
-      </h1>
+    <main>
+      <h1>Thalhalla</h1>
+      <img src={hammerTime} height="242" alt="Thalhammer" className='Thor-center' />
     </main>
   )
 }
 
-export default IndexPage
+// You'll learn about this in the next task, just copy it for now
+export const Head = () => <title>Thalhalla</title>
 
-export const Head = () => <title>Home Page</title>
+// Step 3: Export your component
+export default IndexPage
