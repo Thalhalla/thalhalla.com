698b90e306838ec70141cbe9ce386222478d3b85
Josh Cox <josh@webhosting.coop>
2023-07-18 18:38:16 -0500

    Finishing part5 ready for part6
    Signed-off-by: Josh Cox <josh@webhosting.coop>

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -8,24 +8,42 @@ const BlogPage = ({data}) => {
     <Layout pageTitle="My Blog Posts">
       <ul>
       {
-        data.allFile.nodes.map(node => (
-          <li key={node.name}>
-            {node.name}
-          </li>
+        data.allMdx.nodes.map((node) => (
+          <article key={node.id}>
+            <h2>{node.frontmatter.name}</h2>
+            <p>Posted: {node.frontmatter.datePublished}</p>
+            <p>{node.excerpt}</p>
+          </article>
         ))
       }
       </ul>
     </Layout>
   )
 }
+
 export const query = graphql`
   query {
-    allFile {
+    allMdx(sort: {frontmatter: {datePublished: DESC}}) {
       nodes {
-        name
+        frontmatter {
+          author
+          datePublished(formatString: "MMMM D, YYYY")
+          name
+          slug
+        }
+        id
+        excerpt
+        parent {
+          ... on File {
+            id
+            name
+            modifiedTime
+          }
+        }
       }
     }
-  }`
+  }
+`
 
 export const Head = () => <Seo title="My Blog Posts" />
 
