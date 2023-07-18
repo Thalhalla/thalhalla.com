f4245824ff682062f30e4e1e2bb3aaf92bf597d4
Josh Cox <josh@webhosting.coop>
2023-07-17 21:56:47 -0500

    Finished part2

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,25 @@
-// Step 1: Import React
 import * as React from 'react'
+import { Link } from 'gatsby'
 import hammerTime from "./hammerTime.jpg";
+import Layout from '../components/layout'
 
 // Step 2: Define your component
 const IndexPage = () => {
   return (
+  <Layout pageTitle="Thalhalla">
     <main>
-      <h1>Thalhalla</h1>
       <img src={hammerTime} height="242" alt="Thalhammer" className='Thor-center' />
     </main>
+  </Layout>
   )
 }
 
 // You'll learn about this in the next task, just copy it for now
-export const Head = () => <title>Thalhalla</title>
-
+export const Head = () => (
+  <>
+    <title>Thalhalla</title>
+    <meta name="description" content="Thalhalla Studios" />
+  </>
+)
 // Step 3: Export your component
 export default IndexPage
