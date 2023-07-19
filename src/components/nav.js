7a889c262597f59a23066ddd69decbb9a777f668
Josh Cox <josh@webhosting.coop>
2023-07-18 20:51:43 -0500

    Finishing part6 ready for part7
    Signed-off-by: Josh Cox <josh@webhosting.coop>

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import { Link, useStaticQuery, graphql } from 'gatsby'
+import { Link } from 'gatsby'
 import {
   navLinks,
   navLinkItem,
@@ -7,17 +7,6 @@ import {
 } from './layout.module.css'
 
 const Nav = ({ pageTitle, children }) => {
-  const data = useStaticQuery(graphql`
-    query {
-      site {
-        siteMetadata {
-          title
-          description
-        }
-      }
-    }
-  `)
-
   return (
     <nav>
       <ul className={navLinks}>
