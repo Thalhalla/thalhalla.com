8d2627a7225faf73f801f0822a7e64ebb2dda4e4
Josh Cox <josh@webhosting.coop>
2023-07-29 12:12:45 -0500

    Nav
    Signed-off-by: Josh Cox <josh@webhosting.coop>

---
+++ src/components/Thalhalla.Nav.stories.js
@@ -0,0 +1,22 @@
+import { Nav } from './Nav';
+
+export default {
+  title: 'Thalhalla/Nav',
+  component: Nav,
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
+  tags: ['autodocs'],
+  parameters: {
+    // More on how to position stories at: https://storybook.js.org/docs/react/configure/story-layout
+    layout: 'fullscreen',
+  },
+};
+
+export const LoggedIn = {
+  args: {
+    user: {
+      name: 'Jane Doe',
+    },
+  },
+};
+
+export const LoggedOut = {};
